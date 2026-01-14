f65260caa3e8ffb95c824cccef42669078cec88c
KurzCraft <kurzcraft@example.com>
2026-01-14 23:44:24 +0800

    单独的前端与TTS启动bat

---
+++ start_frontend.bat
@@ -0,0 +1,5 @@
+@echo off
+chcp 65001
+echo Starting VCP Chat Frontend...
+cd /d "%~dp0VCPChat"
+call start.bat
---
+++ start_tts.bat
@@ -0,0 +1,5 @@
+@echo off
+chcp 65001
+echo Starting DMOSpeech2 TTS Service...
+cd /d "%~dp0DMOSpeech2"
+call "运行_自动开启接口服务.bat"
